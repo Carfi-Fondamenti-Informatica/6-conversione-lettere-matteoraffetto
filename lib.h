5e0f3ddcf3ea1e5e468fe7a9968361e6b874657e
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-12-01 12:58:52 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,13 @@
+//
+// Created by matte on 01/12/2021.
+//
+
+#ifndef ESERCITAZIONE6_0_LIB_H
+#define ESERCITAZIONE6_0_LIB_H
+
+ bool Vero_o_Falso (char);
+
+ char Trasformazione (char);
+
+#endif //ESERCITAZIONE6_0_LIB_H
 
