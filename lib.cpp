6d01322327b3451e3e556726ceb2876c9c16913f
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-12-01 12:58:20 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+//
+// Created by matte on 01/12/2021.
+//
+#include "lib.h"
+ bool Vero_o_Falso (char a){
+     if ((a>=65 and a<=90)or(a>=97 and a<=122)){
+         return true;
+     }else{
+         return false;
+     }
+}
 
+ char Trasformazione (char a){
+    if(a>=65 and a <=90) {
+        return a+32;
+    }else if(a>=97 and a<=122){
+        return a-32;
+    }
+ }
